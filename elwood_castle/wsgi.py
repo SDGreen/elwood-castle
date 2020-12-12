f35642527d7a52e3d828a5259d9de5d5458a7434
SDGreen <sad_green@hotmail.co.uk>
2020-12-12 15:59:41 +0000

    initial commit, initial migrations run

---
+++ elwood_castle/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for elwood_castle project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elwood_castle.settings')
+
+application = get_wsgi_application()
