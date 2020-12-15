59fab1c9f1693f94ce25aa4eae2326d234f56f66
SDGreen <sad_green@hotmail.co.uk>
2020-12-15 20:59:29 +0000

    advanced search feature added

--- events/views.py
+++ events/views.py
@@ -1,20 +1,54 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
 
 from .models import Category, Event
 
 
 # Create your views here.
 def all_events(request):
+    """
+    View for returning all or filtered events to the user
+    """
 
     events = Event.objects.all()
     categories = Category.objects.all()
+    selected_category = None
+    query = None
+    sort = None
+
+    if request.method == "GET":
+        if sort in request.GET:
+            sort = request.GET['sort']
+
+            events = events.order_by('price')
+
+        if "category" in request.GET:
+            selected_category = request.GET['category']
+            events = events.filter(category__name=selected_category)
+            try:
+                selected_category = get_object_or_404(Category,
+                                                      name=selected_category)
+            except Exception:
+                pass
+
+        if "q" in request.GET:
+            query = request.GET['q']
+            print(query)
+            filters = (Q(name__icontains=query) |
+                       Q(description__icontains=query) |
+                       Q(category__friendly_name__icontains=query))
+            events = events.filter(filters)
+
+
 
     template = 'events/events.html'
 
     context = {
         'events': events,
-        'categories': categories
+        'categories': categories,
+        'query': query,
+        'selected_category': selected_category,
     }
 
     return render(request, template, context)
