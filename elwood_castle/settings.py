29647e28a6d65e7ec25e63e57c1b24e5efec2f24
SDGreen <sad_green@hotmail.co.uk>
2020-12-27 19:23:51 +0000

    secret key added to enviroment variables

--- elwood_castle/settings.py
+++ elwood_castle/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dzoki1lcv8ig)o1&whyq*om1+s&d87y20l+@9wz&!b1o4l8ro7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEVELOPMENT')
