d7771a6c09e6d6544390d775046cf300ac575b7c
SDGreen <sad_green@hotmail.co.uk>
2020-12-27 21:20:07 +0000

    migrations made on online db

--- elwood_castle/settings.py
+++ elwood_castle/settings.py
@@ -123,17 +123,17 @@ WSGI_APPLICATION = 'elwood_castle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-    }
-else:
-    DATABASES = {
-            'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': BASE_DIR / 'db.sqlite3',
-            }
-        }
+#if 'DATABASE_URL' in os.environ:
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
+#else:
+#    DATABASES = {
+#            'default': {
+#                'ENGINE': 'django.db.backends.sqlite3',
+#                'NAME': BASE_DIR / 'db.sqlite3',
+#            }
+#        }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -191,7 +191,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     AWS_STORAGE_BUCKET_NAME = "elwood-castle"
     AWS_S3_REGION_NAME = "eu-west-2"
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
