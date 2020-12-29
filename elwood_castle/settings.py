0090572181181392cb4ed0b4f8aef7472eb13c0e
SDGreen <sad_green@hotmail.co.uk>
2020-12-29 20:31:32 +0000

    debug settings changed

--- elwood_castle/settings.py
+++ elwood_castle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEVELOPMENT')
+DEBUG = os.environ.get('DEVELOPMENT', False)
 
 ALLOWED_HOSTS = ['elwood-castle.herokuapp.com', 'localhost']
 
