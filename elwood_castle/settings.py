3fe3d6124c2b9678061167944403b1d38f9ecfd5
SDGreen <sad_green@hotmail.co.uk>
2020-12-29 20:37:13 +0000

    attempting to remove debug from being true

--- elwood_castle/settings.py
+++ elwood_castle/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT', False)
+DEBUG = True if os.environ.get('DEVELOPMENT') else False
 
 ALLOWED_HOSTS = ['elwood-castle.herokuapp.com', 'localhost']
 
